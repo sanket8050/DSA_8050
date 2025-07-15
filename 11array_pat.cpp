bdf7f1fe8311f5613147a021c1adc59b5d3e69f5
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-07-16 02:36:27 +0530

    pattern

--- 11array_pat.cpp
+++ 11array_pat.cpp
@@ -42,23 +42,54 @@ SPACE COMPLEXITY: O(1)
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[5]={1,2,3,4,5};
-    int size=5;
-    int start=0;
-    int end=size-1;
-    while(start<=end){
-        if(start>end)
-            break;
-        if(arr[start]==arr[end])                  // odd element  condiotion 
-            cout<<arr[start]<<" ";                // if not used then it print one element double
-        else{
-        cout<<arr[start]<<" ";
-        cout<<arr[end]<<" ";
+// int main(){
+//     int arr[5]={1,2,3,4,5};
+//     int size=5;
+//     int start=0;
+//     int end=size-1;
+//     while(start<=end){
+//         if(start>end)
+//             break;
+//         if(arr[start]==arr[end])                  // odd element  condiotion 
+//             cout<<arr[start]<<" ";                // if not used then it print one element double
+//         else{
+//         cout<<arr[start]<<" ";
+//         cout<<arr[end]<<" ";
             
-        }
-        start++;
-        end--;
-    }
+//         }
+//         start++;
+//         end--;
+//     }
     
+// }
+
+
+
+
+
+int main(){
+    int arr[] = {1,2,2,3,4,1};
+
+    int left = 0;
+    int right = sizeof(arr)/sizeof(arr[0])-1;
+    
+    while(left <= right){
+        if(arr[left] == arr[right]){
+            cout<<arr[left]<<" ";
+        }
+        else{
+            cout<<arr[left]<<" ";
+            cout<<arr[right]<<" ";
+
+
+        }
+        left++;
+        right--;
+    }
+
+   
+
 }
+
+
+
