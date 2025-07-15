feba29bbcf47558c82be95acf215bad05397a4db
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-07-16 02:36:18 +0530

    max

--- 11array_max.cpp
+++ 11array_max.cpp
@@ -29,15 +29,33 @@ SPACE COMPLEXITY: O(1)
 #include<iostream>
 #include<limits.h>                             // library
 using namespace std;
+// int main(){
+    
+//     int maxi = INT_MIN;                         // int_min give the possible smallest value to compare (-[2^31-1])
+//     int arr[]={2,5,78,5,45,86,1,0};
+//     for(int i=0;i<8;i++){
+//         if(arr[i]>maxi){
+//             maxi=arr[i];
+//         }
+//     }
+//     cout<<"max num "<<maxi<<endl;
+    
+// }
+
+
+
+
 int main(){
-    
-    int maxi = INT_MIN;                         // int_min give the possible smallest value to compare (-[2^31-1])
-    int arr[]={2,5,78,5,45,86,1,0};
-    for(int i=0;i<8;i++){
-        if(arr[i]>maxi){
-            maxi=arr[i];
+    int arr[] = {1,3,3,432,21,4,253,21};
+
+    int min = INT_MIN;
+
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        if(arr[i]>min){
+            min = arr[i];
         }
-    }
-    cout<<"max num "<<maxi<<endl;
-    
+        
+     
+    }   
+    cout<<min;
 }
