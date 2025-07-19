a59901a6e8d4bedba7e09dddaf027851041cc3ab
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-07-19 23:57:05 +0530

    S

--- 11array_vector_reserv.cpp
+++ 11array_vector_reserv.cpp
@@ -1,32 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+// #include <iostream>
+// #include <vector>
+// #include <chrono>
+// using namespace std;
+// using namespace chrono;
+
+// int main() {
+//     const int n = 1e8;
+
+//     // --- Without reserve ---
+//     vector<int> noReserve;
+//     auto start1 = high_resolution_clock::now();
+//     for (int i = 0; i < n; ++i) {
+//         noReserve.push_back(i);
+//     }
+//     auto end1 = high_resolution_clock::now();
+//     auto duration1 = duration_cast<milliseconds>(end1 - start1);
+//     cout << "Time without reserve: " << duration1.count() << " ms" << endl;
+
+//     // --- With reserve ---
+//     vector<int> withReserve;
+//     withReserve.reserve(n);  // <-- preallocate memory
+//     auto start2 = high_resolution_clock::now();
+//     for (int i = 0; i < n; ++i) {
+//         withReserve.push_back(i);
+//     }
+//     auto end2 = high_resolution_clock::now();
+//     auto duration2 = duration_cast<milliseconds>(end2 - start2);
+//     cout << "Time with reserve:    " << duration2.count() << " ms" << endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<chrono>
 using namespace std;
 using namespace chrono;
-
-int main() {
-    const int n = 1e8;
-
-    // --- Without reserve ---
-    vector<int> noReserve;
-    auto start1 = high_resolution_clock::now();
-    for (int i = 0; i < n; ++i) {
-        noReserve.push_back(i);
+int main(){
+    int x;
+    auto start = high_resolution_clock::now();
+    for(int i =0;i<1e7;i++){
+        for(int j =0;j<1e8;j++){
+            x++;
+        }
     }
-    auto end1 = high_resolution_clock::now();
-    auto duration1 = duration_cast<milliseconds>(end1 - start1);
-    cout << "Time without reserve: " << duration1.count() << " ms" << endl;
-
-    // --- With reserve ---
-    vector<int> withReserve;
-    withReserve.reserve(n);  // <-- preallocate memory
-    auto start2 = high_resolution_clock::now();
-    for (int i = 0; i < n; ++i) {
-        withReserve.push_back(i);
-    }
-    auto end2 = high_resolution_clock::now();
-    auto duration2 = duration_cast<milliseconds>(end2 - start2);
-    cout << "Time with reserve:    " << duration2.count() << " ms" << endl;
-
-    return 0;
+    auto end = high_resolution_clock::now();
+    auto dur = duration_cast<milliseconds>(end-start);
+    cout << dur.count() ;
 }
