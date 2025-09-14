13b925cd49a1af378cb5eea75c6d0dafd7541101
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-14 23:11:11 +0530

    practice

--- xyzprime.cpp
+++ xyzprime.cpp
@@ -1,17 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
+#include <string>
 using namespace std;
-int main()
-{
-	int n;
-	cout<<"enter num"<<endl;
-	cin>>n;
-	for(int i=2;i<n;i++){
-		if(n%i==0){
-			cout<<"not prime"<<endl;
-		}
-		return 0;
-		cout<<"prime"<<endl;
-	}
-	
+
+string charFrequency(string s) {
+    unordered_map<char, int> freq;
+    
+    // Count frequencies
+    for (char c : s) {
+        freq[c]++;
+    }
+    
+    // Build result string
+    string result = "";
+    for (char c : s) {
+        if (freq[c] != 0) {
+            result += c + to_string(freq[c]);
+            freq[c] = 0; // mark as used
+        }
+    }
+    
+    return result;
 }
 
+int main() {
+    string s = "aabbc";
+    cout << "Output = " << charFrequency(s) << endl;
+}
