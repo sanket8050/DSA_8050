dad45440c6b68b85d9b25c9fc723cb33e403393e
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2025-09-14 23:10:41 +0530

    spiral array

--- 11array.2d.spiral.cpp
+++ 11array.2d.spiral.cpp
@@ -132,3 +132,45 @@ int main(){
     int test2[1][3] = {{1, 2, 3}};
     cout << "1x3 array: 1 2 3" << endl;
 }
+
+// babbar code 
+/*#include<iostream>
+using namespace std;
+
+int spiral(int arr[][3],int col,int row){
+    int left =0 ;int right = row-1;
+    int top = 0; int bottom = col-1;
+    
+    while(left <= right && top <= bottom){
+        for(int i = left;i<=right;i++){
+            cout<<arr[top][i];
+        }
+        top++;
+        for(int i = top ;i<=bottom;i++){
+            cout<<arr[i][right];
+        }
+        right--;
+        if(top<=bottom){
+            for(int i = right;i>=left;i--){
+            cout<<arr[bottom][i];
+        }
+        bottom--;
+        }
+        
+        if(left <= right){
+             for(int i = bottom;i>=top;i--){
+            cout<<arr[i][left];
+        }
+        left++;
+        }
+        
+       
+    }
+    
+    
+    return 0;
+}
+int main(){
+    int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+    spiral(arr,3,3);
+}*/
